59a5460b16dc0b36535d52787e5011d72672b05e
Nest <80919913+nestzin@users.noreply.github.com>
2023-05-27 02:28:28 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// Código JavaScript para o site
+window.addEventListener('DOMContentLoaded', () => {
+    const animePlayer = document.getElementById('animePlayer');
+
+    // Você pode adicionar aqui a lógica para carregar e reproduzir os episódios de animes.
+    // Por exemplo:
+    animePlayer.innerHTML = '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/VIDEO_ID" frameborder="0" allowfullscreen></iframe>';
+});
+
+
+
+
